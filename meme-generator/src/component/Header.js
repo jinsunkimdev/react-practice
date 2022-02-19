1515054d63d914aed8880a0c7dbe76fd7398ad39
jinsunkim <jinsunkim94@gmail.com>
2022-02-20 00:41:17 +0900

    Add README file

--- meme-generator/src/component/Header.js
+++ meme-generator/src/component/Header.js
@@ -1,21 +1,24 @@
-import useState from "react";
+import { useState } from "react";
 import memesData from "../memesData";
 
 const Header = () => {
   const [randomImg, setRandomImg] = useState(
     "https://i.kym-cdn.com/entries/icons/mobile/000/023/920/thumb.jpg"
   );
-  const getMemeImage = () => {
-    const getMemeArr = memesData.data.memes;
-    const getRandomNumber = Math.floor(Math.random() * getMemeArr.length);
-    setRandomImg(getMemeArr[getRandomNumber].url);
+
+  const getMemeImage = (e) => {
+    e.preventDefault();
+    const memeArr = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArr.length);
+    setRandomImg(memeArr[randomNumber].url);
   };
+
   return (
     <main className="header">
       <form>
         <input placeholder="Top text" type="text" className="form--input" />
         <input placeholder="Bottom text" type="text" className="form--input" />
-        <button onClick={getMemeImage} className="form--button">
+        <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🤡
         </button>
         <img src={randomImg} alt="meme" />
